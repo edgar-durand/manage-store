2240e6130fbe1f43ba83d933ad54f2adca54d457
Rafael Perez Hidalgo <rafaelperezbanes@gmail.com>
2021-01-27 21:16:38 +0100

    Eliminando la carpeta api

--- api/app/Api/Permission.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class IsOwnerOrDeny(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return request.user == obj.user
