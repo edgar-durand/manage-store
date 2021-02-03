6b48dc1fbc0cc8a00afbf545d214ebe584aa0539
edgar-durand <edgarduranddiaz@gmail.com>
2021-02-03 14:17:27 -0500

    Restructurados componentes CartList y Checkout en componentes contenedores y representacionales

--- src/components/CartList/CartList.js
+++ src/components/CartList/CartList.js
@@ -1,9 +1,8 @@
 import React, {useState} from "react";
 import store from "../../store";
-import {Link} from "react-router-dom";
-import CartSummary from "../CartSummary/CartSummary";
 import msgNotification from "../../js/msgNotification";
 import toastr from "toastr";
+import CartListUI from "./CartListUI";
 
 const CartList = () => {
     const [products, setProducts] = useState({...store.getState().cart})
@@ -50,98 +49,10 @@ const CartList = () => {
     })
 
 
-    return (
-        <div className="wrapper wrapper-content">
-
-
-            <div className="row">
-                <div className="col-md-9 animated fadeInRight">
-                    <div className="ibox">
-                        <div className="ibox-title">
-                            <span
-                                className="float-right">(<strong>{Object.values(store.getState().cart).length}</strong>) items</span>
-                            <h5>Items in your cart</h5>
-                        </div>
-
-
-                        {
-                            Object.values(products).sort((a, b) => a.id - b.id).map(product => {
-                                return (
-                                    <div className="ibox-content">
-                                        <div className="table-responsive">
-                                            <table className="table shopping-cart-table">
-                                                <tbody>
-                                                <tr>
-                                                    <td width="90">
-                                                        <div>
-                                                            <img style={{
-                                                                objectFit: "contain"
-                                                            }} width="80" height="80" src={product.image} alt=""/>
-                                                        </div>
-                                                    </td>
-                                                    <td className="desc">
-                                                        <h3>
-                                                            <a href="#" className="text-navy">
-                                                                {product.name}
-                                                            </a>
-                                                        </h3>
-                                                        <p className="small">
-                                                            {product.description}
-                                                        </p>
-
-                                                        <div className="m-t-sm">
-                                                            {/*<a href="#" className="text-muted"><i*/}
-                                                            {/*    className="fa fa-gift"/> Add*/}
-                                                            {/*    gift package</a>*/}
-                                                            {/*|*/}
-                                                            <button onClick={() => handleClick(product.id)}
-                                                                    className="btn btn-xs btn-default"><i
-                                                                className="fa fa-trash"/> Remove item
-                                                            </button>
-                                                        </div>
-                                                    </td>
-
-                                                    <td>
-                                                        $ {parseFloat(product.price_cost).toFixed(2)}
-                                                    </td>
-                                                    <td width="65">
-                                                        <input onChange={(event) => handleChange(event, product)}
-                                                               type="text"
-                                                               defaultValue={product.inStock === 0 ? 1 : product.inStock}
-                                                               className="form-control col-12"/>
-                                                    </td>
-                                                    <td>
-                                                        <h4>
-                                                            <label>{((product.inStock === 0 ? 1 : product.inStock) * product.price_cost).toFixed(2)}</label>
-                                                        </h4>
-                                                    </td>
-                                                </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-
-                                    </div>
-                                )
-                            })
-                        }
-
-                        <div className="ibox-content">
-
-                            <Link to="/home/checkout/" className="btn btn-primary float-right"><i
-                                className="fa fa-check-square"/> Checkout
-                            </Link>
-                            <Link to="/home/shopping/" className="btn btn-white"><i
-                                className="fa fa-arrow-left"/> Continue shopping
-                            </Link>
-
-                        </div>
-                    </div>
-                </div>
-                <CartSummary
-                    total={Object.values(store.getState().cart).reduce((a, b) => a + b.price_cost * b.inStock, 0)}
-                />
-            </div>
-        </div>
-    )
+    return <CartListUI
+        products={products}
+        handleClick={(e)=>handleClick(e)}
+        handleChange={(e,p)=>handleChange(e,p)}
+    />
 }
 export default CartList
--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,12 +1,10 @@
-import React, {useState, useRef} from "react"
-import Select from "react-select";
+import React, {useState} from "react"
 import store from "../../store";
-import CartSummary from "../CartSummary/CartSummary";
-import {Link} from "react-router-dom";
 import msgNotification from "../../js/msgNotification";
 import authHelper from "../../js/authHelper";
 import toastr from "toastr";
 import send from "../../js/send";
+import CheckoutUI from "./CheckoutUI";
 
 const Checkout = () => {
     const [state, setState] = useState({
@@ -18,8 +16,7 @@ const Checkout = () => {
         credit: 0,
         total: +Object.values(store.getState().cart).reduce((a, b) => a + b.price_cost * b.inStock, 0)
     });
-    const label = useRef("label");
-    const text = useRef("text");
+
 
     const handleSelect = (e) => {
         if (e)
@@ -77,74 +74,12 @@ const Checkout = () => {
     }
 
 
-    if (label.current.classList)
-        if (state.credit >= state.total) {
-            text.current.innerHTML = " <i style='font-size: 30px' class='fa fa-check text-info text-right'/>"
-            label.current.classList.remove("text-warning")
-            label.current.classList.add("text-success")
-        } else {
-            text.current.innerHTML = " <i style='font-size: 30px' class='fa fa-close text-danger text-right'/>"
-            label.current.classList.remove("text-success")
-            label.current.classList.add("text-warning")
-        }
-
-    return (
-        <div className="wrapper wrapper-content">
-            <div className="row">
-                <div className="col-md-9 animated fadeInRight">
-                    <div className="ibox">
-                        <div className="ibox-title">
-                            <span
-                                className="float-right">(<strong>{Object.values(store.getState().cart).length}</strong>) items purchased</span>
-                            <h5>Checkout</h5>
-                        </div>
-                        <div className="ibox-content">
-                            <label className="col-12 form-inline"> Account
-                                credit:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <h5 className="text-center"
-                                                                                ref={label}> $ {state.credit.toFixed(2)} </h5>
-                                <p ref={text}></p>
-                            </label>
-                            <div className="col-12">
-                                <Select
-                                    options={
-                                        Object.values(store.getState().accounts).map(x => ({
-                                                value: x.id,
-                                                label: `${x.name} ${x.description}`
-                                            })
-                                        )
-                                    }
-                                    isClearable={true}
-                                    placeholder="Select account"
-                                    name="accounts"
-                                    onChange={(e) => handleSelect(e)}
-                                />
-                            </div>
-                            <br/>
-                            <label className="col-12 form-inline">
-                                Description
-                            </label>
-                            <textarea onChange={(event) => handleChange(event)} className="form-control"
-                                      name="description"/>
-
-                        </div>
-                        <div className="ibox-content">
-
-                            <button onClick={() => handleCheckout()} className="btn btn-primary float-right col-auto"><i
-                                style={{fontSize: "20px"}}
-                                className="fa fa-check"/> Checkout
-                            </button>
-                            <Link to="/home/shopping/" className="btn btn-white"><i
-                                className="fa fa-arrow-left"/> Continue shopping
-                            </Link>
-
-                        </div>
-                    </div>
-                </div>
-                <CartSummary
-                    total={state.total}
-                />
-            </div>
-        </div>
-    )
+    return <CheckoutUI
+        credit={state.credit}
+        total={state.total}
+        handleSelect={(e)=>handleSelect(e)}
+        handleChange={(e)=>handleChange(e)}
+        handleCheckout={()=>handleCheckout()}
+    />
 }
 export default Checkout
